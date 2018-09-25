5b746ebb596ab1eb47517e4d93ac5f080b219589
zhu-ty <zhu-ty@outlook.com>
2018-09-25 16:34:30 +0800

    added scripts

---
+++ Multiple_Decoder.sh
@@ -0,0 +1,21 @@
+
+printf "" > tmp.txt
+count=0;
+for i in `ls *.bin`
+do
+    if ((count % 2 == 0)); then
+        #echo 0;
+        #echo "$i 0\n" >> tmp.txt
+        printf "./DecoderGPU $i 0 20\n" >> tmp.txt
+    else
+        #echo 1;
+        printf "./DecoderGPU $i 1 20\n" >> tmp.txt
+    fi
+    ((count++));
+done
+
+#cat tmp.txt | xargs -L 1 -P 4 -I {} echo {} | cut -d' ' -f1,2 | xargs -n 2 sh -c './DecoderGPU $0 $1'
+
+cat tmp.txt | xargs -L 1 -P 2 -I xiaoyun sh -c "xiaoyun"
+
+rm tmp.txt
